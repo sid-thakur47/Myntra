84da510ed541908963d3a7362a4ab622f62a265d
sid-thakur47 <sidthakur258@gmail.com>
2020-08-04 22:00:56 +0530

    ADDED:Base class for Initialization of test cases

---
+++ Myntra/MyntraBase/Base.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Myntra.MyntraBase
+{
+    public class Base
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Initilize()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.myntra.com/login/password";
+        }
+    }
+}

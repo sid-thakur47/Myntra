0428cb05eb2d198dc42939ad0768c17462857cd5
sid-thakur47 <sidthakur258@gmail.com>
2020-08-04 22:04:17 +0530

    ADDED:Functionality to get user credentails from json file

---
+++ Myntra/JsonReader/JsonReader.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Myntra.Reader
+{
+    public class JsonReader
+    {
+        public string email = "";
+        public string password = "";
+        public string json = "";
+
+        public JsonReader()
+        {
+            using (StreamReader r = new StreamReader("C:\\Users\\Shivani\\Desktop\\Backup\\credentials.json"))
+            {
+                json = r.ReadToEnd();
+            }
+            dynamic array = JsonConvert.DeserializeObject(json);
+            email = array["email"];
+            password = array["password"];
+        }
+    }
+}

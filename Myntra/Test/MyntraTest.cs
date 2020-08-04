832c826876c66f3451dd5302442257cc5c6e77d4
sid-thakur47 <sidthakur258@gmail.com>
2020-08-04 22:05:30 +0530

    ADDED:Automation script for Myntra Login

---
+++ Myntra/Test/MyntraTest.cs
@@ -0,0 +1,25 @@
+﻿using Myntra.MyntraBase;
+using Myntra.Pages;
+using NUnit.Framework;
+
+namespace Myntra.Test
+{
+    [TestFixture]
+    public class MyntraTest : Base
+    {
+
+        [Test]
+        public void LoginTest()
+        {
+            Login login = new Login(driver);
+            login.MyntraLogin();
+            Assert.AreEqual("Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra", driver.Title);
+        }
+
+        [OneTimeTearDown]
+        public void QuitBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}

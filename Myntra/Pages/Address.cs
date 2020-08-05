acb88a2fd784feb101377a940900e959e10aa34d
sid-thakur47 <sidthakur258@gmail.com>
2020-08-05 17:32:55 +0530

    Page object to store all Address page web elements

---
+++ Myntra/Pages/Address.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Myntra.Pages
+{
+    public class Address
+    {
+        public IWebDriver driver;
+
+        public Address(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//div[@id='placeOrderButton']")]
+        public IWebElement continueButton;
+
+        public void SelectAddress()
+        {
+            continueButton.Click();
+        }
+    }
+}

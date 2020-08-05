d3f3cea7cbf4c2f680b7d34a0f2a904a08174d1b
sid-thakur47 <sidthakur258@gmail.com>
2020-08-05 20:01:03 +0530

    ADDED:Functionality to take screenshot and generate report

---
+++ Myntra/ExtentReport/ReportManager.cs
@@ -0,0 +1,29 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System.Net;
+
+namespace Myntra.ExtentReport
+{
+    public class ReportManager
+    {
+        private static ExtentReports extent;
+        private static ExtentHtmlReporter htmlReporter;
+
+        public static ExtentReports GetInstance()
+        {
+            if (extent == null)
+            {
+                string reportPath = @"C:\Users\Shivani\source\repos\Myntra\Myntra\ExtentReport\Report.html";
+                htmlReporter = new ExtentHtmlReporter(reportPath);
+                extent = new ExtentReports();
+                extent.AttachReporter(htmlReporter);
+                string hostname = Dns.GetHostName();
+                extent.AddSystemInfo("OS", "Windows 10");
+                extent.AddSystemInfo("Host Name", hostname);
+                extent.AddSystemInfo("Browser", "Chrome");
+                extent.AddSystemInfo("Environment", "QA");
+            }
+            return extent;
+        }
+    }
+}
---
+++ Myntra/Utils/Utility.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myntra.Utils
+{
+    public class Utility
+    {
+        public static string TakeScreenshot(IWebDriver driver, string testStatus)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            Screenshot screenshot = ts.GetScreenshot();
+            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string finalPath = path.Substring(0, path.LastIndexOf("bin")) + "Screenshots\\" + testStatus + ".png";
+            string localPath = new Uri(finalPath).LocalPath;
+            screenshot.SaveAsFile(localPath, ScreenshotImageFormat.Png);
+            return localPath;
+        }
+    }
+}

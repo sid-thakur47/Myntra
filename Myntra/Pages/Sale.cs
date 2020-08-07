4f25fac5aadd8c7f97d116241de9a839e929c265
sid-thakur47 <sidthakur258@gmail.com>
2020-08-07 23:43:22 +0530

    ADDED:Automation script for Myntra sale

---
+++ Myntra/Pages/Sale.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Myntra.Pages
+{
+    /// <summary>
+    /// Store all web elements of sale page
+    /// </summary>
+    public class Sale
+    {
+        public IWebDriver driver;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Sale"/> class
+        /// </summary>
+        /// <param name="driver">to control browser</param>
+        public Sale(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        /// <summary>
+        /// Web elements
+        /// </summary>
+        [FindsBy(How = How.XPath, Using = "//div[3]//div[1]//div[1]//div[1]//div[1]//div[2]//div[1]" +
+                                           "//div[1]//div[1]//div[1]//div[1]//a[1]//div[1]//picture[1]//img[1]")]
+        public IWebElement men;
+
+        [FindsBy(How = How.XPath, Using = "//div[3]//div[1]//div[1]//div[1]//div[1]//div[3]//div[1]" +
+                                             "//div[1]//div[1]//div[1]//div[1]//a[1]//div[1]//picture[1]//img[1]")]
+        public IWebElement viewAll;
+
+        public void BuyFromSale()
+        {
+            men.Click();
+            Thread.Sleep(5000);
+            viewAll.Click();
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Myntra/Test/MyntraSaleTest.cs
@@ -0,0 +1,81 @@
+﻿using Myntra.MyntraBase;
+using Myntra.Pages;
+using NUnit.Framework;
+
+namespace Myntra.Test
+{
+    public class MyntraSaleTest : Base
+    {
+        /// <summary>
+        /// Login to myntra account 
+        /// </summary>
+        [Test, Order(1)]
+        public void LoginTest()
+        {
+            Login login = new Login(driver);
+            login.MyntraLogin();
+            Assert.AreEqual(loginTitle, driver.Title);
+        }
+
+        [Test, Order(2)]
+        public void BuyProductFromSaleTest()
+        {
+            Sale sal = new Sale(driver);
+            sal.BuyFromSale();
+            Assert.AreEqual(sale, driver.Title);
+        }
+
+        /// <summary>
+        /// Select product
+        /// </summary>
+        [Test, Order(3)]
+        public void SelectProductFromSaleTest()
+        {
+            Product product = new Product(driver);
+            product.SelectProduct(1);
+            //Assert.IsTrue(product.Validation().Displayed);
+        }
+
+        /// <summary>
+        /// add shirt to shopping bag
+        /// </summary>
+        [Test, Order(4)]
+        public void AddingSaleProductToShoppingBagTest()
+        {
+            ShoppingBag shop = new ShoppingBag(driver);
+            shop.AddToBagWhenOffer();
+            Assert.AreEqual(shopping, driver.Url);
+        }
+
+        /// <summary>
+        /// Select address
+        /// </summary>
+        [Test, Order(5)]
+        public void AddressTest()
+        {
+            Address addr = new Address(driver);
+            addr.SelectAddress();
+            Assert.AreEqual(addressTitle, driver.Title);
+        }
+
+        /// <summary>
+        /// Logout from application
+        /// </summary>
+        [Test, Order(6)]
+        public void LogoutFromSaleTest()
+        {
+            Logout log = new Logout(driver);
+            log.LogoutMyntra();
+            Assert.IsTrue(log.Validation().Displayed);
+        }
+
+        /// <summary>
+        /// Close the browser
+        /// </summary>
+        [OneTimeTearDown]
+        public void QuitBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}

38eaf59434911b0cc2278d6c537f06140c835a13
sid-thakur47 <sidthakur258@gmail.com>
2020-08-07 23:40:16 +0530

    REFACTORED:To place order only when product has offer price

--- Myntra/Pages/ShoppingBag.cs
+++ Myntra/Pages/ShoppingBag.cs
@@ -6,6 +6,9 @@
 
 using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace Myntra.Pages
@@ -36,11 +39,11 @@ namespace Myntra.Pages
         [FindsBy(How = How.XPath, Using = "//span[contains(text(),'GO TO BAG')]")]
         public IWebElement goToBag;
 
-        [FindsBy(How = How.XPath, Using = "//div[contains(@class,'itemComponents-base-quantity')]")]
-        public IWebElement quantity;
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/div/div/div/div/div/div/div[1]/div[4]/div/div/div/div/div[2]/div/div[4]/div[1]/div")]
+        public IWebElement dPrice;
 
-        [FindsBy(How = How.XPath, Using = "//div[@id='2']")]
-        public IWebElement changeQuantitiy;
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/div/div/div/div/div/div/div[1]/div[4]/div/div/div/div/div[2]/div/div[4]/div[2]/span[1]/span")]
+        public IWebElement oPrice;
 
         [FindsBy(How = How.XPath, Using = "//div[contains(text(),'Place Order')]")]
         public IWebElement placeOrder;
@@ -51,14 +54,35 @@ namespace Myntra.Pages
         public void AddToBag()
         {
             addToBag.Click();
+            goToBag.Click();
+            placeOrder.Click();
+        }
+
+        public void AddToBagWhenOffer()
+        {
+            addToBag.Click();
             Thread.Sleep(1000);
             goToBag.Click();
-            Thread.Sleep(1000);
-            quantity.Click();
-            Thread.Sleep(1000);
-            changeQuantitiy.Click();
-            Thread.Sleep(1000);
-            placeOrder.Click();
+            Thread.Sleep(10000);
+
+            string discount = dPrice.Text;
+            string discountFilter = Regex.Replace(discount, "[^0-9]", "");
+            string d = string.Join("", discountFilter.ToCharArray().Where(Char.IsDigit));
+
+            string orignal = oPrice.Text;
+            string orignalFilter = Regex.Replace(orignal, "[^0-9]", "");
+            string o = string.Join("", orignalFilter.ToCharArray().Where(Char.IsDigit));
+
+            int orignalPrice = int.Parse(o);
+            Console.WriteLine("OrignalPrice:" + orignalPrice);
+
+            int discountPrice = int.Parse(d);
+            Console.WriteLine("discount:" + discountPrice);
+
+            if (discountPrice < orignalPrice)
+            {
+                placeOrder.Click();
+            }
         }
     }
 }
